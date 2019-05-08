6720460e78e8501cc27e7726f58e5a7c0b63dc98
Steven Tu <stu562@gmail.com>
2019-05-08 16:32:23 -0700

    merges third branch

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 console.log("hello world");
 function newFunctionBranch(){
-<<<<<<< HEAD
-  console.log('i made this commit from master')
-}
-=======
   console.log('well sure, i definately made these changes from newbranch');
   console.log('this is getting messy, shall we rebase?');
   console.log('lets make more commits first');
   console.log('i made this change in my repo lets see git pull --rebase');
 }
->>>>>>> thirdbranch
