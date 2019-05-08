ea568be777a7ae64ed59e6682b67d7e7381345e5
Steven Tu <stu562@gmail.com>
2019-05-08 16:18:46 -0700

    add console log

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("hello world");
 function newFunctionBranch(){
-  console.log('well sure, i definately made these changes from newbranch')
+  console.log('well sure, i definately made these changes from newbranch');
+  console.log('this is getting messy, shall we rebase?')
 }
