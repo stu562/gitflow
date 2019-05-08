75851692b0a1e0e1e3e0e365d45b3b832d3f6323
Steven Tu <stu562@gmail.com>
2019-05-08 16:23:24 -0700

    adds more coding lines

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ function newFunctionBranch(){
   console.log('well sure, i definately made these changes from newbranch');
   console.log('this is getting messy, shall we rebase?');
   console.log('lets make more commits first');
+  console.log('i made this change in my repo');
 }
