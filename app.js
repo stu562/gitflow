21b7b05ddecdd9a036da3bd2ca4fa0cac216d844
Steven Tu <stu562@gmail.com>
2019-05-08 16:21:27 -0700

    added another consolelog

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("hello world");
 function newFunctionBranch(){
   console.log('well sure, i definately made these changes from newbranch');
-  console.log('this is getting messy, shall we rebase?')
+  console.log('this is getting messy, shall we rebase?');
+  console.log('lets make more commits first');
 }
